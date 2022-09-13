d987763d0d755d2ee59bb965be8451771e9be12d
Aditya-raj77x <ar8342077@gmail.com>
2022-09-13 10:13:58 -0400

    added some logoc in herocraouser

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,11 +11,45 @@ import DeafultHOC from "../Layouts/Deafult.Layout"
 
 const HomePage = () => {
   const [recomondedMovies, setRecomondedMovies] = useState([]);
-  const [premiermovies, setPremierMovies]= useState([]);
-  const [onlineStreamEvents, setOnlineStreamEvents]=useState([]);
+  const [premiermovies, setPremierMovies] = useState([]);
+  const [onlineStreamEvents, setOnlineStreamEvents] = useState([]);
 
   return (
-    <div>HomePage</div>
+    <>
+      <HeroCarousel />
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <h1 className='text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3'>
+          The Best of Entertainment
+        </h1>
+        <EntertaimentCard />
+
+      </div>
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <PosterSlider title="Recommended Movies"
+          subject="List of recommended movies"
+          posters={recomondedMovies} 
+          isDark={false}/>
+      </div>
+      <div className='bg-premier-800 py-12'>
+        <div className='container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+          <div className='hidden md:flex'>
+            <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+             alt="Rupay" className='w-fill h-full' />
+          </div>
+          <PosterSlider title="Premiers"
+          subject="Brand New Releases Every Friday"
+          posters={premiermovies} 
+          isDark={true}/>
+
+        </div>
+      </div>
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+      <PosterSlider title="Online Streaming Events"
+          subject=""
+          posters={onlineStreamEvents} 
+          isDark={false}/>
+      </div>
+    </>
   )
 }
 
--- src/component/HeroCarousel/HeroCarousel.jsx
+++ src/component/HeroCarousel/HeroCarousel.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+//slider
+import HeroSlider from "react-slick"
 
 const HeroCarousel = () => {
-  return (
-    <div>HeroCarousel</div>
-  )
+
+    const [images, setImages] = useState([]);
+    return (
+        <>
+            <div className='lg:hidden'>
+                <HeroSlider>
+                    {
+                        images.map((image)=>(
+                            <div className='w-fill h-56 md:h-80 py-3'>
+                                <img src="" alt=" Hero Banner" className='w-full h-full rounded-md object-cover' />
+                            </div>
+                        ))
+                    }
+                </HeroSlider>
+            </div>
+            <div className='hidden lg:block'></div>
+
+        </>
+    )
 }
 
 export default HeroCarousel
