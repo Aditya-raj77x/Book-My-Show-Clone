ed98a7d32a87dc3cb800de5eb5ca822ae3615b0c
Aditya-raj77x <ar8342077@gmail.com>
2022-09-13 07:29:08 -0400

    more in the setup section

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+//component
+import HeroCarousel from '../component/HeroCarousel/HeroCarousel'
+import EntertaimentCard from '../component/Entertrntment/EntertaimentCard'
+import PosterSlider from '../component/PostSlider/PosterSlider'
+
+// Layout HOC
 import DeafultHOC from "../Layouts/Deafult.Layout"
 
+
+
 const HomePage = () => {
+  const [recomondedMovies, setRecomondedMovies] = useState([]);
+  const [premiermovies, setPremierMovies]= useState([]);
+  const [onlineStreamEvents, setOnlineStreamEvents]=useState([]);
+
   return (
     <div>HomePage</div>
   )
