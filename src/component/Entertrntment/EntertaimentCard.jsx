bcb2e09ac61fa3d16130246fda2864cbbfc49673
Aditya-raj77x <ar8342077@gmail.com>
2022-09-15 03:47:29 -0400

    completed navbar of home page

--- src/component/Entertrntment/EntertaimentCard.jsx
+++ src/component/Entertrntment/EntertaimentCard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+//slick
+import {Slider} from "react-slick";
 
-const EntertaimentCard = () => {
+const EntertaimentCard = (props) => {
   return (
-    <div>EntertaimentCard</div>
+    <>
+      <div className='w-full h-30 px-2'>
+        <img 
+        src={props.src} 
+        alt=""  
+        className='w-full h-full rounded-lg'/>
+      </div>
+    </>
   )
 }
 
